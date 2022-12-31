e33d3374479d2c106e05ae65b0da9b72be241dec
Max <maxbui0911@gmail.com>
2022-12-31 22:08:08 +0100

    Docker

--- src/main/java/com/project/foodDelivery/repository/RestaurantsRepository.java
+++ src/main/java/com/project/foodDelivery/repository/RestaurantsRepository.java
@@ -2,12 +2,14 @@ package com.project.foodDelivery.repository;
 
 
 import com.project.foodDelivery.model.Restaurant;
+import jakarta.persistence.Entity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Entity
 @Repository
 public interface RestaurantsRepository extends MongoRepository<Restaurant, String> {
 //    @Query
